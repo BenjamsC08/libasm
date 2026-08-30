5ef1e0bda3fcde52e358a9532f9770a7b52ac35f
bidule <machin@gmx.com>
2026-08-30 04:43:40 +0200

    yes

--- Bonus/ft_list_remove_if_bonus.s
+++ Bonus/ft_list_remove_if_bonus.s
@@ -11,18 +11,15 @@ section .text
 ; rcx > ptr func free > r14
 
 ft_list_remove_if:
-	push rbx
 	push r12
 	push r13
 	push r14
 	push r15
-	sub rsp, 8
 
-	mov rbx, rdi
 	mov r12, rsi
 	mov r13, rdx
 	mov r14, rcx
-	mov r15, rbx	  
+	mov r15, rdi	  
 
 .loop:
 	mov rcx, [r15]	  ; Node
@@ -54,12 +51,10 @@ ft_list_remove_if:
 
 
 .end:
-	add rsp, 8
 	pop r15
 	pop r14
 	pop r13
 	pop r12
-	pop rbx
 	xor rax, rax
 	ret
 
